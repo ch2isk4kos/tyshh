f0c1b143a3fd23d5fe8f3fc7c602777e8790040f
ch2isk4kos <ch2isk4kos@gmail.com>
2019-03-19 22:13:55 -0400

    added comment deletion functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find_by(params[:post_id])
+        @post = Post.find(params[:post_id])
         @comment = @post.comments.find(params[:id])
         @comment.destroy
 
